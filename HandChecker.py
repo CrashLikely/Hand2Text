cd981d53bbaff3190cdcfe25cde09223208610c1
BuildTools <23bootha@gmail.com>
2022-12-21 09:51:43 -0500

    Fixed unable to train bug

--- HandChecker.py
+++ HandChecker.py
@@ -3,29 +3,44 @@ import tensorflow as tf
 from tensorflow import keras
 from keras import layers
 import numpy as np
+import matplotlib.pyplot as plt
 
 data = pd.read_pickle("files/saved_data.pickle")
 
-data_values = data["value"]
-data_landmarks = tf.convert_to_tensor(data["landmarks"].values)
+data_values = np.asarray(data["num_value"].values.tolist())
+print(data_values.shape)
+print(data_values[5])
+
+
+data_landmarks = np.asarray(data.landmarks.values.tolist())
+print(data_landmarks.shape)
 print(type(data_landmarks))
+print(type(data_values[0]))
 #Model
 model = keras.Sequential(name="HandPoseChecker")
+model.add(layers.Dense(200,activation="relu", name="layer1"))
+model.add(keras.layers.GlobalAveragePooling1D())
+model.add(layers.Dense(100,activation="relu",name="layer2"))
 model.add(layers.Flatten())
-model.add(layers.Dense(2,activation="relu", name="layer1"))
-model.add(layers.Dense(3,activation="relu",name="layer2"))
-model.add(layers.Dense(4,name="layer3"))
+model.add(layers.Dense(26,name="layer3"))
 
 #x = tf.ones(1,21,2)
 #y = model(x)
 
-model.compile(optimizer = keras.optimizers.Adam(),
-loss=keras.losses.SparseCategoricalCrossentropy(),
-metrics =[keras.metrics.SparseCategoricalAccuracy()])
+model.compile(optimizer = 'adam',
+loss='MeanSquaredError',
+metrics =['accuracy'])
 
 history = model.fit(
     data_landmarks,
     data_values,
     batch_size=50,
-    epochs=10
+    epochs=500
 )
+model.summary()
+
+plt.plot(history.history['accuracy'])
+plt.title('model accuracy')
+plt.ylabel('accuracy')
+plt.xlabel('epoch')
+plt.show()
--- datacleaner.py
+++ datacleaner.py
@@ -4,9 +4,36 @@ import numpy as np
 
 df  = pd.read_pickle("files/saved_data.pickle")
 #for i in range(len(df)):
-#    for elem in (df["landmarks"][i]):
+#    for elem in (df["landm arks"][i]):
 #        df["landmarks"][i] = np.asarray(df["landmarks"][i])
 #df.to_pickle("files/saved_data.pickle")
 
-print(df)
-
+#print(df["value"][0])
+#for i in range(len(df)):
+#    if df["value"][i] == 'A':df["num_value"][i]=1
+#    if df["value"][i] == 'B':df["num_value"][i]=2
+#    if df["value"][i] == 'C':df["num_value"][i]=3
+#    if df["value"][i] == 'D':df["num_value"][i]=4
+#    if df["value"][i] == 'E':df["num_value"][i]=5
+#    if df["value"][i] == 'F':df["num_value"][i]=6
+#    if df["value"][i] == 'G':df["num_value"][i]=7
+#    if df["value"][i] == 'H':df["num_value"][i]=8
+#    if df["value"][i] == 'I':df["num_value"][i]=9
+#    if df["value"][i] == 'J':df["num_value"][i]=10
+#    if df["value"][i] == 'K':df["num_value"][i]=11
+#    if df["value"][i] == 'L':df["num_value"][i]=12
+#    if df["value"][i] == 'M':df["num_value"][i]=13
+#    if df["value"][i] == 'N':df["num_value"][i]=14
+#    if df["value"][i] == 'O':df["num_value"][i]=15
+#    if df["value"][i] == 'P':df["num_value"][i]=16
+#    if df["value"][i] == 'Q':df["num_value"][i]=17
+#    if df["value"][i] == 'R':df["num_value"][i]=18
+#    if df["value"][i] == 'S':df["num_value"][i]=19
+#    if df["value"][i] == 'T':df["num_value"][i]=20
+#    if df["value"][i] == 'U':df["num_value"][i]=21
+#    if df["value"][i] == 'V':df["num_value"][i]=22
+#    if df["value"][i] == 'W':df["num_value"][i]=23
+#    if df["value"][i] == 'X':df["num_value"][i]=24
+#    if df["value"][i] == 'Y':df["num_value"][i]=25
+#    if df["value"][i] == 'Z':df["num_value"][i]=26
+#df.to_pickle("files/saved_data.pickle")
